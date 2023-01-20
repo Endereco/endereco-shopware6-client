d0dcd5c7ab62329819cf76fc37004dedc9bede91
Michał Daniel <michal.daniel@digital-masters.de>
2023-01-20 12:03:49 +0100

    Street splitter - prepared database structure

---
+++ src/Entity/EnderecoAddressExtension/EnderecoAddressExtensionEntity.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Endereco\Shopware6Client\Entity\EnderecoAddressExtension;
+
+use Shopware\Core\Checkout\Customer\Aggregate\CustomerAddress\CustomerAddressEntity;
+use Shopware\Core\Framework\DataAbstractionLayer\Entity;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityIdTrait;
+
+class EnderecoAddressExtensionEntity extends Entity
+{
+    use EntityIdTrait;
+
+    protected string $addressId;
+    protected string $street;
+    protected string $houseNumber;
+
+    protected ?CustomerAddressEntity $address = null;
+
+    public function getAddressId(): string
+    {
+        return $this->addressId;
+    }
+
+    public function setAddressId(string $addressId): void
+    {
+        $this->addressId = $addressId;
+    }
+
+    public function getStreet(): string
+    {
+        return $this->street;
+    }
+
+    public function setStreet(string $street): void
+    {
+        $this->street = $street;
+    }
+
+    public function getHouseNumber(): string
+    {
+        return $this->houseNumber;
+    }
+
+    public function setHouseNumber(string $houseNumber): void
+    {
+        $this->houseNumber = $houseNumber;
+    }
+
+    public function getAddress(): ?CustomerAddressEntity
+    {
+        return $this->address;
+    }
+
+    public function setAddress(?CustomerAddressEntity $address): void
+    {
+        $this->address = $address;
+    }
+
+}
