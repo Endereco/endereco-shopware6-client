5ff505654542431a226204af05fc46f8dbb2e1c9
Ilja Weber <ilja@endereco.de>
2023-03-28 13:39:37 +0200

    Refactor ApiTestController
    Added some comments and improved readability. Added a missing paramenter to checkAPICredentials method.

--- src/Controller/Api/ApiTestController.php
+++ src/Controller/Api/ApiTestController.php
@@ -11,30 +11,52 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
- * This one is for SW Version < 6.4.11.0
- * @RouteScope(scopes={"api"})
+ * ApiTestController handles API requests for testing API credentials.
  *
- * This one is for SW Version >= 6.4.11.0
- * @Route(defaults={"_routeScope"={"api"}})
+ * @author Ilja Weber
+ * @package Endereco\Shopware6Client\Controller\Api
+ *
+ * @RouteScope(scopes={"api"}) // For SW Version < 6.4.11.0
+ * @Route(defaults={"_routeScope"={"api"}}) // For SW Version >= 6.4.11.0
  */
 class ApiTestController extends AbstractController
 {
+    /**
+     * @var EnderecoService
+     */
     private EnderecoService $enderecoService;
 
+    /**
+     * ApiTestController constructor.
+     *
+     * @param EnderecoService $enderecoService
+     */
     public function __construct(EnderecoService $enderecoService)
     {
         $this->enderecoService = $enderecoService;
     }
 
-    public function checkAPICredentials(Request $request): JsonResponse
+    /**
+     * Check API credentials by making a request using provided credentials.
+     *
+     * @param Request $request The incoming HTTP request.
+     * @param Context $context Context data of the sales channel/user.
+     *
+     * @return JsonResponse Returns a JsonResponse with a 'success' key indicating whether the credentials are valid.
+     */
+    public function checkAPICredentials(Request $request, Context $context): JsonResponse
     {
         $apiKey = $request->get('EnderecoShopware6Client.config.enderecoApiKey', '');
         $endpointUrl = $request->get('EnderecoShopware6Client.config.enderecoRemoteUrl', '');
+
+        // Check if the API key and endpoint URL are not empty
         if (empty(trim($apiKey)) || empty(trim($endpointUrl))) {
             return new JsonResponse(['success' => false]);
         }
-        return  new JsonResponse([
-            'success' => $this->enderecoService->checkApiCredentials($endpointUrl, $apiKey, $context)
-        ]);
+
+        // Call the checkApiCredentials method of the EnderecoService
+        $result = $this->enderecoService->checkApiCredentials($endpointUrl, $apiKey, $context);
+
+        return new JsonResponse(['success' => $result]);
     }
 }
