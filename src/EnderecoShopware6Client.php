ed45fae154f6556c76792ee1fa7052cc4c73ebd5
Ilja Weber <ilja@endereco.de>
2023-06-30 13:19:35 +0200

    Cleaned up and documented main plugin class and the installer

--- src/EnderecoShopware6Client.php
+++ src/EnderecoShopware6Client.php
@@ -9,10 +9,24 @@ use Shopware\Core\Framework\Plugin\Context\InstallContext;
 use Shopware\Core\Framework\Plugin\Context\UpdateContext;
 use Endereco\Shopware6Client\Installer\PluginLifecycle;
 
+/**
+ * Class EnderecoShopware6Client
+ *
+ * Main plugin class for the Endereco Shopware 6 client.
+ *
+ * @package Endereco\Shopware6Client
+ */
 class EnderecoShopware6Client extends Plugin
 {
     /**
-     * @throws Exception
+     * Uninstall the plugin.
+     *
+     * This function is used to handle the uninstallation process of the plugin.
+     * It calls the parent's uninstall function and the PluginLifecycle's uninstall function.
+     *
+     * @param UninstallContext $uninstallContext The context of the uninstall process.
+     * @throws Exception If there is any error during the uninstall process.
+     * @return void
      */
     public function uninstall(UninstallContext $uninstallContext): void
     {
@@ -21,6 +35,16 @@ class EnderecoShopware6Client extends Plugin
         (new PluginLifecycle($this->container))->uninstall($uninstallContext);
     }
 
+    /**
+     * Install the plugin.
+     *
+     * This function is used to handle the installation process of the plugin.
+     * It calls the parent's install function and the PluginLifecycle's install function.
+     *
+     * @param InstallContext $installContext The context of the install process.
+     *
+     * @return void
+     */
     public function install(InstallContext $installContext): void
     {
         parent::install($installContext);
@@ -28,6 +52,16 @@ class EnderecoShopware6Client extends Plugin
         (new PluginLifecycle($this->container))->install();
     }
 
+    /**
+     * Update the plugin.
+     *
+     * This function is used to handle the update process of the plugin.
+     * It calls the parent's update function and the PluginLifecycle's update function.
+     *
+     * @param UpdateContext $updateContext The context of the update process.
+     *
+     * @return void
+     */
     public function update(UpdateContext $updateContext): void
     {
         parent::update($updateContext);
