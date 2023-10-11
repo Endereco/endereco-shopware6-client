0847623cd364c1b0e0cc339b17ba85ac577388ca
Ilja Weber <ilja@endereco.de>
2023-10-11 11:39:40 +0200

    Fix the bug with not nullable extension fields
    With the 1.4.0 we made housenumber and street not nullable. However the Doctrine, that Shopware ist using internally, seems to transform an empty string into a null. So when the user tries to save an address
    that doesn't have a house number, therefore the "housenumber" is "", it gets transformed into a "NULL", and causes an SQL Exception, when tryging to write a NULL to a non nullable field.
    
    So to fix it, we made the fields nullable.

---
+++ src/Migration/Migration1678865789MakeStreetAndHousenumberNullableAgain.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Endereco\Shopware6Client\Migration;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Exception;
+use Shopware\Core\Framework\Migration\MigrationStep;
+
+class Migration1678865789MakeStreetAndHousenumberNullableAgain extends MigrationStep
+{
+    public function getCreationTimestamp(): int
+    {
+        return 1678865789;
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function update(Connection $connection): void
+    {
+        $sql = <<<SQL
+            ALTER TABLE `endereco_address_ext` 
+            MODIFY `street` VARCHAR(255) NULL DEFAULT '', 
+            MODIFY `house_number` VARCHAR(255) NULL DEFAULT '';
+        SQL;
+        $connection->executeStatement($sql);
+    }
+
+    public function updateDestructive(Connection $connection): void
+    {
+        // implement update destructive
+    }
+}
