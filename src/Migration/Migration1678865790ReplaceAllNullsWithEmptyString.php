1d132a1d400ce7cc294a9c79b579dce8ecdaa86e
Ilja Weber <ilja@endereco.de>
2023-10-24 16:41:50 +0200

    Fix bug with non existing property in validation requestdatabag

---
+++ src/Migration/Migration1678865790ReplaceAllNullsWithEmptyString.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Endereco\Shopware6Client\Migration;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Exception;
+use Shopware\Core\Framework\Migration\MigrationStep;
+
+class Migration1678865790ReplaceAllNullsWithEmptyString extends MigrationStep
+{
+    public function getCreationTimestamp(): int
+    {
+        return 1678865790;
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function update(Connection $connection): void
+    {
+        // Update rows where 'street' is NULL
+        $connection->executeStatement("
+            UPDATE `endereco_address_ext` 
+            SET `street` = '' 
+            WHERE `street` IS NULL
+        ");
+
+        // Update rows where 'house_number' is NULL
+        $connection->executeStatement("
+            UPDATE `endereco_address_ext` 
+            SET `house_number` = '' 
+            WHERE `house_number` IS NULL
+        ");
+    }
+
+    public function updateDestructive(Connection $connection): void
+    {
+        // implement update destructive
+    }
+}
