25053ef2a4d270b92cb30bbd5ae1b71bab96dde9
Ilja Weber <ilja@endereco.de>
2024-01-18 21:26:14 +0100

    Remove javacsript comments in build script

--- build.sh
+++ build.sh
@@ -27,6 +27,18 @@ find ./EnderecoShopware6ClientStore -type f -exec sed -i 's/enderecoshopware6cli
 mv ./EnderecoShopware6ClientStore/src/EnderecoShopware6Client.php ./EnderecoShopware6ClientStore/src/EnderecoShopware6ClientStore.php
 mv ./EnderecoShopware6ClientStore/src/Resources/public/administration/js/endereco-shopware6-client.js ./EnderecoShopware6ClientStore/src/Resources/public/administration/js/endereco-shopware6-client-store.js
 
+
+# Find all Twig files and process them
+TWIG_FILES_PATH="./EnderecoShopware6ClientStore/src/Resources/views"
+find "$TWIG_FILES_PATH" -type f -name "*.twig" | while read -r file; do
+    # Remove single-line JavaScript comments
+    sed -i '/\/\/.*/d' "$file"
+
+    # Remove multi-line JavaScript comments
+    # This is more complex and might not work correctly for all cases
+    sed -i '/\/\*\*/,/\*\//d' "$file"
+done
+
 zip -r EnderecoShopware6ClientStore-$branch.zip EnderecoShopware6ClientStore
 rm -rf EnderecoShopware6ClientStore
 
@@ -45,6 +57,15 @@ rm EnderecoShopware6Client/endereco.scss
 rm EnderecoShopware6Client/package.json
 rm EnderecoShopware6Client/package-lock.json
 rm EnderecoShopware6Client/webpack.config.js
+TWIG_FILES_PATH="./EnderecoShopware6Client/src/Resources/views"
+find "$TWIG_FILES_PATH" -type f -name "*.twig" | while read -r file; do
+    # Remove single-line JavaScript comments
+    sed -i '/\/\/.*/d' "$file"
+
+    # Remove multi-line JavaScript comments
+    # This is more complex and might not work correctly for all cases
+    sed -i '/\/\*\*/,/\*\//d' "$file"
+done
 
 zip -r EnderecoShopware6Client-$branch.zip EnderecoShopware6Client
 rm -rf EnderecoShopware6Client
