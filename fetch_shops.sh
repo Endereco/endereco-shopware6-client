87c0d8f57977e4ff3762731bab1299bb8be37d5d
Ilja Weber <ilja@endereco.de>
2025-01-07 11:48:32 +0100

    Only download shop version when needed

--- fetch_shops.sh
+++ fetch_shops.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-
 # Define an array of versions
 versions=("6.4.0.0" "6.4.1.2" "6.4.2.1" "6.4.3.1" "6.4.4.1" "6.4.5.1" "6.4.6.1" "6.4.7.0" "6.4.8.2" "6.4.9.0" "6.4.10.1" "6.4.11.1" "6.4.12.0" "6.4.13.0" "6.4.14.0" "6.4.15.2" "6.4.16.1" "6.4.17.2" "6.4.18.1" "6.4.19.0" "6.4.20.2")
 
 # Loop through each version
 for version in "${versions[@]}"; do
+    # Check if directory already exists
+    if [ -d "./shops/$version" ]; then
+        echo "Skipping version $version - directory already exists"
+        continue
+    fi
+
     echo "Processing version $version..."
     
     # Define image name
@@ -15,9 +20,8 @@ for version in "${versions[@]}"; do
     
     # Start a temporary container
     container_id=$(docker run -d --rm "$image_name" tail -f /dev/null)
-
+    
     # Create target directory for shop files
-    rm -rf "./shops/$version"
     mkdir -p "./shops/$version"
     
     # Copy files from container to host
@@ -31,5 +35,4 @@ for version in "${versions[@]}"; do
     
     echo "Completed processing for version $version"
 done
-
 echo "All versions processed successfully."
